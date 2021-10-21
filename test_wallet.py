47e09b59bd8eb8ba84a50f6d330eaf46a8c52f84
Rachel Harris <rachelharris@MacBook-Air.local>
2021-10-21 12:11:38 -0500

    Added baseline coding to start wallet class unit test.

--- test_wallet.py
+++ test_wallet.py
@@ -1,2 +1,22 @@
 import unittest
+import wallet
+import customer
 
+
+class TestWalletMoney(unittest.TestCase):
+    """ Tests for instantiating a Wallet object, test that its money list has a len of 88"""
+
+
+    def setUp(self):
+        self.customer = wallet()
+
+    def test_instan_wallet_object(self):
+
+
+
+
+
+
+# Wallet class:
+# 1. fill_wallet
+# a. Instantiate a Wallet object, test that its money list has a len of 88
