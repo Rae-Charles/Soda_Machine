c5d0cdb279c5b079f3c738043f7423ad94a14afe
Rachel Harris <rachelharris@MacBook-Air.local>
2021-10-21 14:31:55 -0500

    Adjusted syntax on wallet unit test file.

--- test_wallet.py
+++ test_wallet.py
@@ -2,18 +2,17 @@ import unittest
 from wallet import Wallet
 
 
-
 class TestWalletMoney(unittest.TestCase):
     """ Tests for instantiating a Wallet object, test that its money list has a len of 88"""
 
 
     def setUp(self):
-        self.customer = Wallet()
+        self.wallet = Wallet()
 
    
     def __init__(self):
         """ Testing for instantiating a Wallet object, test that its money list has a len of 88 """
-        money_list = len(self.customer.fill_wallet)
+        money_list = len(self.wallet.fill_wallet)
         self.assertEqual(len(self.money_list), money_list, '88')
 
 
